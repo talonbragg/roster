77e667dbb6e13c88cbc752ac303052ee50301bf1
talonbragg <pvpgamecrash@gmail.com>
2017-03-27 20:42:51 -0700

    Fixes to if statements

--- script/form.js
+++ script/form.js
@@ -10,41 +10,45 @@ var numVal = num.value;
 var playerArr = [];
 
 function formSubmit(e) {
-	// Debugging
-	console.log('submit event', e);
-	console.log(nam.length);
-	console.log(playerText);
-	console.log(mainDebug);
+    if (playerName.length > 0 && playerNum.length > 0) {
+        // Debugging
+        console.log('submit event', e);
+        console.log(nam.length);
+        console.log(playerText);
+        console.log(mainDebug);
 
-	// Event Handling
-	e.stopPropagation();
-	e.preventDefault();
+        // Event Handling
+        e.stopPropagation();
+        e.preventDefault();
 
-	// Object
-	var playerName = nam.value;
-	var playerNum = num.value;
-	var thisPlayer = {name: playerName, num: playerNum};
+        // Object
+        var playerName = nam.value;
+        var playerNum = num.value;
+        var thisPlayer = {
+            name: playerName,
+            num: playerNum
+        };
 
-	var player = document.createElement("LI");
-	var playerText = document.createTextNode(nam.value + ' ' + '#' + num.value);
+        var player = document.createElement("LI");
+        var playerText = document.createTextNode(nam.value + ' ' + '#' + num.value);
 
-	// Array additio
-	var mainDebug = playerArr.push(thisPlayer);
+        // Array additio
+        var mainDebug = playerArr.push(thisPlayer);
 
-	player.appendChild(playerText);
-	roster.appendChild(player);
+        player.appendChild(playerText);
+        roster.appendChild(player);
 
-	if (playerName.length < 1) {
-		err.innerHTML = "Please enter a player name!";
-	}
-	else if (playerNum.length < 1) {
-		err.innerHTML = "Please enter a player number!"
-	}
+        // Debugging
+        console.log(nam.length);
+        console.log(playerText);
+        console.log(mainDebug);
+        console.log(playerArr);
+    } else if (playerName.length < 1) {
+        err.innerHTML = "Please enter a player name!";
+    } else if (playerNum.length < 1) {
+        err.innerHTML = "Please enter a player number!"
+    }
 
-	// Debugging
-	console.log(nam.length);
-	console.log(playerText);
-	console.log(mainDebug);
-	console.log(playerArr);
+
 };
 form.addEventListener('submit', formSubmit);
